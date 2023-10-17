4e13311b99426ea631aa5bd3b5f63dd316a732df
Ricky Clegg <ricky.clegg@transferwise.com>
2023-10-17 16:18:13 +0100

    feat: add UserService

---
+++ src/main/java/com/example/UserService.java
@@ -0,0 +1,29 @@
+package com.example;
+
+import java.util.List;
+
+public class userService {
+    private List<String> users;
+
+    // Constructor injection
+    public UserService(List<String> users) {
+        this.users = users;
+    }
+
+    // Rest of the code...
+
+    public boolean isUserExists(String userName) {
+        boolean f = false;
+
+        for (int index = 0; index < users.size(); index++) {
+            String user = users.get(index);
+
+            if (userName.equals(user)) {
+                f = true;
+                break;
+            }
+        }
+
+        return f;
+    }
+}
