42cd8804178b17e55742f63db8b154a499194eb4
Yijun Lin <yijun.lin@wise.com>
2025-10-22 23:46:58 +0100

    test copilot instruction

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,22 @@
+import java.sql.*;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+        // This is just to test copilot working as exoected
+        String userInput = args.length > 0 ? args[0] : "admin";
+        String query = "SELECT * FROM users WHERE username = '" + userInput + "'";
+        
+        try {
+            Connection conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/mydb", "root", "password123");
+            Statement stmt = conn.createStatement();
+            ResultSet rs = stmt.executeQuery(query);
+            
+            while (rs.next()) {
+                System.out.println("User: " + rs.getString("username"));
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
     }
 }
